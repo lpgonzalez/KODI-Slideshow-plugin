52b223a5febf713f7e9248a79f114ecda0ed1fdf
Lisardo Prieto González <lisardo.prieto@datos101.com>
2022-04-17 13:57:46 +0200

    Added plugin settings and translations
    Added settings to specify the path of resources for the slideshow, and if they should be played in random order.

--- script.slideshow/default.py
+++ script.slideshow/default.py
@@ -1,6 +1,14 @@
 import xbmc
+import xbmcaddon
 
 
 # entry point
 if (__name__ == '__main__'):
-    xbmc.executebuiltin('RecursiveSlideShow(/storage/pictures)')
+    addonSettings = xbmcaddon.Addon(id='script.slideshow')
+    slideshow_path = addonSettings.getSetting('slideshow_path')
+    slideshow_random = addonSettings.getSetting('slideshow_random').lower() == 'true'
+
+    if slideshow_random:
+        xbmc.executebuiltin('SlideShow(' + slideshow_path + ',recursive, random)')
+    else:
+        xbmc.executebuiltin('SlideShow(' + slideshow_path + ',recursive, notrandom)')
